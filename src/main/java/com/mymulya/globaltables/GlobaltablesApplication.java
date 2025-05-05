ecefbf9d3c431a2bf9a718e262a593e3cca81454
SasiKDP <sasank9110@gmail.com>
2025-05-05 16:33:17 +0530

    Initial commit with Spring Boot microservice project

---
+++ src/main/java/com/mymulya/globaltables/GlobaltablesApplication.java
@@ -0,0 +1,13 @@
+package com.mymulya.globaltables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobaltablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobaltablesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymulya/globaltables/GlobaltablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymulya.globaltables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobaltablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
